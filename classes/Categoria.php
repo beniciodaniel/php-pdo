fcab93522bb1182e62e800a08404eca7eaaedcae
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-05 01:04:49 -0300

    Implementa o atualizar editar

--- classes/Categoria.php
+++ classes/Categoria.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'classes/Conexao.php';
+
 class Categoria
 {
 
@@ -9,16 +11,37 @@ class Categoria
     public function listar()
     {
         $query = "SELECT id, nome FROM categorias";
-        $conexao = new PDO('mysql:host=127.0.0.1;dbname=estoque', 'root', 'password');
+        $conexao = Conexao::pegarConexao();
         $resultado = $conexao->query($query);
         $lista = $resultado->fetchAll();
         return $lista;
     }
 
+
+    public function carregar()
+    {
+        $query = "SELECT id, nome FROM categorias WHERE id=" . $this->id;
+        $conexao = Conexao::pegarConexao();
+        $resultado = $conexao->query($query);
+        $lista = $resultado->fetchAll();
+        foreach ($lista as $linha){
+            return $linha;
+        }
+    }
+    
+    
     public function inserir()
     {
         $query = "INSERT INTO categorias (nome) VALUES ('" . $this->nome . "')";
-        $conexao = new PDO('mysql:host=127.0.0.1;dbname=estoque', 'root', 'password');
+        $conexao = Conexao::pegarConexao();
+        $conexao->exec($query);
+    }
+
+
+    public function atualizar()
+    {
+        $query = "UPDATE categorias set nome = '" . $this->nome ."' WHERE id =  " . $this->id;
+        $conexao = Conexao::pegarConexao();
         $conexao->exec($query);
     }
 }
