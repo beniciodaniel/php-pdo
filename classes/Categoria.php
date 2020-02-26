b8f36cddf6183e2e4465eff41f2b99a294d9045e
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-26 10:31:41 -0300

    Cria a página de criação de Produtos

--- classes/Categoria.php
+++ classes/Categoria.php
@@ -16,9 +16,9 @@ class Categoria
     }
 
 
-    public function listar()
+    public static function listar()
     {
-        $query = "SELECT id, nome FROM categorias";
+        $query = "SELECT id, nome FROM categorias ORDER BY nome";
         $conexao = Conexao::pegarConexao();
         $resultado = $conexao->query($query);
         $lista = $resultado->fetchAll();
